0a7d2e82484f36666e0c98882a3d19625bcb8f06
userxx1000 <u202118264@upc.edu.pe>
2025-07-27 18:18:46 -0500

    feat: change api base

--- frontend/src/environment/environment.js
+++ frontend/src/environment/environment.js
@@ -2,8 +2,8 @@
 //export const DEBUG_MODE = import.meta.env.VITE_DEBUG_MODE === 'true';
 
 
-export const API_BASE_URL_PRODUCTS = 'http://localhost:5000';
-export const API_BASE_URL_ORDERS = 'http://localhost:5000';
+export const API_BASE_URL_PRODUCTS = 'https://backend-production-102c.up.railway.app';
+export const API_BASE_URL_ORDERS = 'https://backend-production-102c.up.railway.app';
 
 
 export const DEBUG_MODE = import.meta.env.VITE_DEBUG_MODE === 'true';
